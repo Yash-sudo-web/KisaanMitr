d9f8cc86733739cd31afd708e8dc77b39ae1571d
harsh mahajan <127186841+HarshMN2345@users.noreply.github.com>
2023-06-03 12:09:25 +0530

    Update HeadPage.js

--- client/src/components/HeadPage.js
+++ client/src/components/HeadPage.js
@@ -364,6 +364,56 @@ export default function HeadPage() {
                 </div>
             </div>
         </div>
+<section className="bg-white border-spacing-2 border-s-violet-50">
+  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+    <div className="mx-auto max-w-screen-md text-center">
+      <h2 className="mb-4 text-3xl font-extrabold text-center text-amber-400 sm:text-4xl">
+        Sign up for our newsletter
+      </h2>
+      <p className=" text-amber-500 text-center md:mb-12 sm:text-xl">
+        Stay up to date with the roadmap progress, announcements, and exclusive discounts.
+        Feel free to sign up with your email.
+      </p>
+      <form action="#">
+        <div className="flex flex-col items-center space-y-4 max-w-screen-sm sm:flex-row sm:space-y-0">
+          <div className="relative w-full items-center">
+            <label htmlFor="email" className="hidden mb-2 text-sm font-medium text-gray-900">
+              Email address
+            </label>
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+              </svg>
+            </div>
+            <input
+              className="block w-full p-3 pl-10 text-right text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500"
+              placeholder="Enter your email"
+              type="email"
+              id="email"
+              required=""
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="px-5 py-3 w-full text-sm font-medium text-amber-500 rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Subscribe
+            </button>
+          </div>
+        </div>
+        <div className="mx-auto max-w-screen-sm mt-4 text-sm text-gray-500 newsletter-form-footer">
+          We care about the protection of your data.{" "}
+          <a href="#" className="font-medium text-black dark:text-primary-500 hover:underline">
+            Read our Privacy Policy
+          </a>
+          .
+        </div>
+      </form>
+    </div>
+  </div>
+</section>
         <div className=" bg-amber-900 text-white py-8 px-4" id="login">
   <div className="max-w-4xl mx-auto flex justify-between items-center">
     <div className="w-1/2">
