a2ef48110ea2049c1159cd30c05dc073b00c168d
Yash-sudo-web <ymathur123@gmail.com>
2023-06-11 19:53:57 +0530

    updated

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -75,7 +75,7 @@ export const User = () => {
           <br />
           <img src={getRandomImage(loadingImages)} alt="Loading" />
           <br />
-          <p className="w-1/3 text-center text-white">If it's taking longer then 10 seconds, please click here to <button onClick={reload}>Reload</button></p>
+        <button className="text-center text-amber-400" onClick={reload}>Reload</button>
         </p>
       </div>
       </>
