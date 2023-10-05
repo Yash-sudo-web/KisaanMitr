5dcdbaa543ea4841f3efe3d88d91e946d2590b8c
Shaurya10101 <shauryagupta8046@gmail.com>
2023-10-05 23:13:53 +0530

    Market Tabs vertical

--- client/src/components/Tabs.js
+++ client/src/components/Tabs.js
@@ -6,15 +6,15 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import ProductCard from './ProductCard';
 
-function CustomTabPanel(props) {
+function TabPanel(props) {
     const { children, value, index, ...other } = props;
 
     return (
         <div
             role="tabpanel"
             hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
             {...other}
         >
             {value === index && (
@@ -26,7 +26,7 @@ function CustomTabPanel(props) {
     );
 }
 
-CustomTabPanel.propTypes = {
+TabPanel.propTypes = {
     children: PropTypes.node,
     index: PropTypes.number.isRequired,
     value: PropTypes.number.isRequired,
@@ -34,14 +34,13 @@ CustomTabPanel.propTypes = {
 
 function a11yProps(index) {
     return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
     };
 }
 
-export default function BasicTabs() {
+export default function VerticalTabs() {
     const [value, setValue] = React.useState(0);
-
     const products = [
         {
             id: 1,
@@ -91,35 +90,53 @@ export default function BasicTabs() {
             price: "Rs 5/Kg"
         }
     ]
-
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
 
     return (
-        <Box sx={{ width: '100%' }}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Tabs value={value} onChange={handleChange} 
-                textColor="secondary"
-                indicatorColor="secondary"
-                aria-label="secondary tabs example"
-                centered>
-                    <Tab label="Item One" {...a11yProps(0)} />
-                    <Tab label="Item Two" {...a11yProps(1)} />
-                    <Tab label="Item Three" {...a11yProps(2)} />
-                </Tabs>
-            </Box>
-            <CustomTabPanel value={value} index={0}>
+        <Box
+            sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}
+        >
+            <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                aria-label="Vertical tabs example"
+                sx={{ borderRight: 1, borderColor: 'divider' }}
+            >
+                <Tab label="Item One" {...a11yProps(0)} />
+                <Tab label="Item Two" {...a11yProps(1)} />
+                <Tab label="Item Three" {...a11yProps(2)} />
+                <Tab label="Item Four" {...a11yProps(3)} />
+                <Tab label="Item Five" {...a11yProps(4)} />
+                <Tab label="Item Six" {...a11yProps(5)} />
+                <Tab label="Item Seven" {...a11yProps(6)} />
+            </Tabs>
+            <TabPanel value={value} index={0}>
                 {products.map((product) =>
                     <ProductCard key={product.id} product={product} />
                 )}
-            </CustomTabPanel>
-            <CustomTabPanel value={value} index={1}>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
                 Item Two
-            </CustomTabPanel>
-            <CustomTabPanel value={value} index={2}>
+            </TabPanel>
+            <TabPanel value={value} index={2}>
                 Item Three
-            </CustomTabPanel>
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+                Item Four
+            </TabPanel>
+            <TabPanel value={value} index={4}>
+                Item Five
+            </TabPanel>
+            <TabPanel value={value} index={5}>
+                Item Six
+            </TabPanel>
+            <TabPanel value={value} index={6}>
+                Item Seven
+            </TabPanel>
         </Box>
     );
 }
